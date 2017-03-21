b1ac822ad6cc83c1a3fcc26ade2302e1898c4303
JeffBship <jeffrey.blankenship@my.ccsu.edu>
2017-03-21 18:12:53 -0400

    Tuesday 6pm

--- P2P_fileshare/src/p2p_fileshare/P2P_fileshare.java
+++ P2P_fileshare/src/p2p_fileshare/P2P_fileshare.java
@@ -1,8 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Jeff Blankenship, Adrian Ward-Manthey
+ * CS 490 Final Project
+ * Prof Williams
+ * 
+ * This is some test code to send and receive 128 byte strings via sockets.
  */
+
+
 package p2p_fileshare;
 
 /**

bdd8c224366b0517a2cc6de3ba5f2dd992fe4c6a
JeffBship <jeffrey.blankenship@my.ccsu.edu>
2017-04-03 23:17:19 -0400

    Added sharing folder

---
+++ P2P_fileshare/src/p2p_fileshare/files/I_hate_git.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * GIT KEEPS DELETING MY FILE WHEN I COMMIT
+ * 
+ * 
+ * Packet format is:  
+ * peerID [0-2] \sp[3] IPaddress[4-19] \n[20] sequence[21-26] \sp[27] packetsRemaining[28-33] \n[34] data[35-126] \n[127]
+ * 
+ * This is not a DataGram Packet.  Packets are packed into DataGram Packets when sending.
+ * 
+ * 
+ * 
+ */
---
+++ P2P_fileshare/src/p2p_fileshare/files/Peer.java
@@ -0,0 +1,19 @@
+package p2p_fileshare.files;
+
+import java.io.IOException;
+
+public class Peer {
+  
+  public static void main(String[] args) throws IOException {
+    
+    String destinationIP = "192.168.1.46";  //Jeff desktop localIP
+    //String destinationIP = "192.168.1.118";  //Jeff laptop localIP
+    int destinationPort = 55000;
+    
+    
+    System.out.println(" hello there");
+    
+        
+  }
+  
+}

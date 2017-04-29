dfc1bdf243762c0f4ee9ca92b449752f4ae378b5
Adrian Alucard <wartinator@gmail.com>
2017-04-29 00:34:38 -0400

    TCP DONE (needs more testing)...need to integrate with user interaction

--- P2P_fileshare/src/p2p_fileshare/TCPClient.java
+++ P2P_fileshare/src/p2p_fileshare/TCPClient.java
@@ -1,21 +1,21 @@
 package p2p_fileshare;
 import java.io.*;
 import java.net.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * Simple client connects sends a sentence periodically and outputs the
  * response. This is an adaption of the code provided by the Computer
  * Networking: A Top Down Approach book by Kurose and Ross
- *
+ * 
  * @author Chad Williams
  */
 public class TCPClient extends Thread {
 
   private int serverPort;
-  
-
+  int bytesRead;
+  int current = 0;
+  FileOutputStream outFile = null;
+  BufferedOutputStream buffOut = null;
   public TCPClient(String name, int serverPort) {
     super(name);
     this.serverPort = serverPort;
@@ -27,61 +27,44 @@ public class TCPClient extends Thread {
   @Override
   public void run() {
     Socket clientSocket = null;
-    FileOutputStream oStream = null;
-    BufferedOutputStream buff = null;
-    int bytesRead;
-    int current = 0;
     try {
       
-      //UPDATE DAMMIT
-      //LATER ON GET THIS DESTINATION FROM PEER Folder itself
       
-     
-     File fDestination = new File("C:\\Users\\Surface Book\\Desktop");
-   
-      if (!fDestination.canRead()) {
-           fDestination.setReadable(true);
-      }
-      oStream = new FileOutputStream(fDestination);
-      //Receive File
-      byte[] array = new byte[3000000];
       System.out.println("CLIENT opening socket");
-      clientSocket = new Socket("192.168.2.7", serverPort);
+      //INSERT IP HERE
+      clientSocket = new Socket("192.168.1.96", serverPort);
       System.out.println("CLIENT connected to server");
-      InputStream in = clientSocket.getInputStream();
-     //wrapping oStream inside of buff is faster
-      oStream = new FileOutputStream(fDestination);
-      buff = new BufferedOutputStream(oStream);
-      bytesRead = in.read(array, 0, array.length);
+      
+      //get file
+      byte[] bRecv = new byte[9447213];
+      InputStream is = clientSocket.getInputStream();
+      File f = new File("C:\\Users\\Owner\\Music\\Test\\test.m4a");
+      if(!f.exists())
+          f.createNewFile();
+      outFile = new FileOutputStream(f);
+      buffOut = new BufferedOutputStream(outFile);
+      bytesRead = is.read(bRecv, 0, bRecv.length);
       current = bytesRead;
-      
+      int available = -1;
+      while((available = is.read(bRecv))>0){
+          buffOut.write(bRecv, 0, available);
+      }
+      /*
       do{
-        bytesRead = in.read(array, 0, array.length-current);
-        if(bytesRead >=0) 
-          current +=bytesRead;
-      } while(bytesRead>-1);
-      
-      buff.write(array, 0, current);
-      buff.flush();
-      System.out.println("File Received");
-      
-       /*
-      BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
-      System.out.println("CLIENT opening socket");
-      clientSocket = new Socket("192.168.2.32", serverPort);
-      System.out.println("CLIENT connected to server");
-      
+        bytesRead = is.read(bRecv, current, bRecv.length-current);
+        if(bytesRead>=0)
+            current+=bytesRead;
+        
+      }while(bytesRead > -1);
+*/
+      //buffOut.write(bRecv, 0, current);
       //DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-      BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-      int songBytes = inFromServer.read();
-      oStream.write(songBytes);
-      System.out.println("Received file!");
-              
-      
-      */
-      //Thread.sleep(180000);
+     
+     // buffOut.write(bRecv, 0, current);
+      buffOut.flush();
+      buffOut.close();
+      System.out.println("Closing Connection");
       clientSocket.close();
-      oStream.close();
       System.out.println(this.getName() + " closed connection to server");
     } catch (Exception e) {
       e.printStackTrace();
@@ -92,7 +75,6 @@ public class TCPClient extends Thread {
       } catch (Exception cse) {
         // ignore exception here
       }
-      
-    }  
+    }
   }
 }
--- P2P_fileshare/src/p2p_fileshare/TCPServer.java
+++ P2P_fileshare/src/p2p_fileshare/TCPServer.java
@@ -8,16 +8,15 @@ import java.nio.file.Files;
  * Simple TCP server thread that starts up and waits for TCP connections and
  * echos what is sent capitalized. This is an adaption of the code provided by
  * the Computer Networking: A Top Down Approach book by Kurose and Ross
- *
+ * 
+ * This class wile be used to SEND files as a stream of bytes in a bufferedoutputstream
+ * 
  * @author Chad Williams
  */
 public class TCPServer extends Thread {
 
   private int port;
-  public FileInputStream inStream=null;
-  public BufferedInputStream buff = null;
-  public OutputStream out = null;
-  
+
   public TCPServer(String name, int port) {
     super(name);
     this.port = port;
@@ -26,63 +25,53 @@ public class TCPServer extends Thread {
   /**
    * Start the thread to begin listening
    */
-  @Override
-  public void run()   {
+  public void run() {
     ServerSocket serverSocket = null;
+    File outFile = null;
+    FileInputStream fis = null;
+    BufferedInputStream buffRead = null;
+    OutputStream out = null;
+    byte[] bSend;    
     try {
-      //String clientSentence;
-      //String capitalizedSentence;
       serverSocket = new ServerSocket(this.port);
+     
       while (true) {
         System.out.println("SERVER accepting connections");
         Socket clientConnectionSocket = serverSocket.accept();
         System.out.println("SERVER accepted connection (single threaded so others wait)");
         
-        //NEED TO SOMEHOW GET FILE NAME FROM THE QUERY
         
         
-        // This is regarding the server state of the connection
-        //while (clientConnectionSocket.isConnected() && !clientConnectionSocket.isClosed()) {
-            //byte[] array = Files.readAllBytes(new File("C:\\Users\\Surface Book\\Desktop\\Music\\Silver.mp3").toPath());
-        File fSend = (new File("C:\\Users\\Surface Book\\Desktop\\Music\\Break.mp4"));
-        byte[] array = new byte[(int)fSend.length()];
-        inStream = new FileInputStream(fSend);
-        buff = new BufferedInputStream (inStream);
-        buff.read(array, 0, array.length);
-        out = clientConnectionSocket.getOutputStream();
-        System.out.println("Send file... ");
-        out.write(array, 0, array.length);
-        out.flush();
+        outFile = new File("C:\\Users\\Surface Book\\Desktop\\Music\\Silver.mp3");
         
-     //   }
-        System.out.println("FINISHED");
+        bSend = new byte[(int) outFile.length()];
+        //Should read all bytes from file into bSend
+        //bSend = Files.readAllBytes(outFile.toPath());
         
-        /*
+        
+        fis = new FileInputStream(outFile);
+        buffRead = new BufferedInputStream(fis);
+        buffRead.read(bSend, 0, bSend.length);
+        
+        
+        
         // This is regarding the server state of the connection
         while (clientConnectionSocket.isConnected() && !clientConnectionSocket.isClosed()) {
-          BufferedReader inFromClient = new BufferedReader(new InputStreamReader(clientConnectionSocket.getInputStream()));
-          DataOutputStream outToClient = new DataOutputStream(clientConnectionSocket.getOutputStream());
-          //clientSentence = inFromClient.readLine();
-          // Note if this returns null it means the client closed the connection
-          if (array != null) {
-            //System.out.println("SERVER Received: " + clientSentence);
-            //capitalizedSentence = clientSentence.toUpperCase() + '\n';
-           // System.out.println("SERVER responding: " + capitalizedSentence);
-           System.out.println("SERVER responding: \n");
-           int len;
-           int n = 0;
-           while (-1 != (n = bis.read(array)))  {
-              outToClient.write(array, 0, n);
-           }
-            //outToClient.write(array);      
-            System.out.println("Just Sent the Byte Array");
+           out =  clientConnectionSocket.getOutputStream();
+            // Note if this returns null it means the client closed the connection
+          if (out!= null) {
+            System.out.println("SERVER sending file...");
+            System.out.println("Size is" + bSend.length);
+            out.write(bSend, 0, bSend.length);
+            out.flush();
+            clientConnectionSocket.close();
+            System.out.println("Done");
             
           } else {
             clientConnectionSocket.close();
             System.out.println("SERVER client connection closed");
           }
         }
-        */
       }
     } catch (Exception e) {
       e.printStackTrace();
@@ -94,9 +83,5 @@ public class TCPServer extends Thread {
         }
       }
     }
- 
-    //FINALLY????
   }
 }
-
-   
